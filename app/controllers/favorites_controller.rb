51e3b6130e9865fc23c31eb90bcf34242aecd84f
弥和 <a.cokegasuki@gmail.com>
2021-09-10 21:08:28 +0900

    お気に入り機能のコントローラー作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+    def create
+        #current_userに紐づいたいいねを作成
+        @favorite = current_user.favorites.create(recipe_id: paramas[:recipe_id])
+        redirect_back(fallback_location: root_path)
+    end
+
+    def destroy
+        #どのレシピに紐づいたものか探す
+        @recipe = Recipe.find(params[:recipe_id])
+        #上記で設定した変数のIDをrecipe_idに渡す(ログイン中のユーザーのお気に入り)
+        @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+        @favorite.destroy
+        #一つ前のURLが見つからなかったらroot_pathに戻る
+        redirect_back(fallback_location: root_path)
+    end
+end
