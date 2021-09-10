8d4ea9c7bf891a8055e435ce174aba8c81faa944
弥和 <a.cokegasuki@gmail.com>
2021-09-10 16:45:54 +0900

    ユーザー詳細画面とルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   root to: 'home#top'
   devise_for :users
+
+  #exceptで不要なルート設定を外す
+  resource :user, except: [:new, :create, :destory]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
