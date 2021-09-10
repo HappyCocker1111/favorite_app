d809b1d9c1419c1662cdc3e62b2417daf5577318
弥和 <a.cokegasuki@gmail.com>
2021-09-10 20:55:12 +0900

    アソシエーションの追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
     belongs_to :user
+    has_many :favorites, dependent: :destory
 
     with_options presence: true do
         validates :title
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   
   has_many :recipes, dependent: :destroy
+  has_many :favorites, dependent: :destory
 end
